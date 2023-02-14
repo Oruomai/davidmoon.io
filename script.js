f50c4b35a8371e74a632a7a4f170e92e41d253ad
David Moon <hrmoon99@gmail.com>
2023-02-14 16:16:14 -0600

    1.0.1

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-  if (entry.isIntersecting) {
-  if (entry.target.classList.contains('hidden')) {
-  entry.target.classList.add('show');
-  entry.target.addEventListener('transitionend', () => {
-  entry.target.classList.remove('hidden');
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains('hidden')) {
+        entry.target.classList.add('show');
+        entry.target.addEventListener('transitionend', () => {
+          entry.target.classList.remove('hidden');
+        });
+      }
+    }
   });
-  }
-  }
-  });
-  });
-  
-  const hiddenElements = document.querySelectorAll('.hidden')
-  hiddenElements.forEach((el) => observer.observe(el));
-  
-  const sidebar = document.querySelector(".sidebar");
-  
-  window.addEventListener("scroll", () => {
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+const sidebar = document.querySelector(".sidebar");
+
+window.addEventListener("scroll", () => {
   if (window.pageYOffset > 70) {
-  sidebar.classList.add("visible");
+    sidebar.classList.add("visible");
   } else {
-  sidebar.classList.remove("visible");
+    sidebar.classList.remove("visible");
   }
 });
